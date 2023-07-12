6a4e9884606a972cf6ed09b4227514bb7f1ebcce
soybean15 <marlonpadilla1593@gmail.com>
2023-07-12 18:11:34 +0800

    added admin view

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,58 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/home/HomeView.vue'
-import Login from '../views/home/auth/LoginView.vue'
-import Register from '../views/home/auth/RegisterView.vue'
+
 
 const routes = [
+
+  {
+    path: '/admin',
+    name: 'admin',
+    component: HomeView
+
+
+  },
+  {
+    path: '/',
+    name: 'main',
+    redirect: '/home',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/MainView'),
+    children: [
+
+      {
+        path: '/home',
+        name: 'main',
+        component:HomeView
+
+      },
+      {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+      },
+
+    ]
+
+
+  },
+
+  {
+    path: '/admin',
+    name: 'admin',
+    component:()=> import('@/views/admin/AdminView')
+  },
+
+
   {
     path: '/home',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+
+
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: Login
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: Register
-  },
+
 ]
 
 const router = createRouter({
--- src/views/components/LeftSideBar.vue
+++ src/views/components/LeftSideBar.vue
@@ -1,87 +1,124 @@
 <template>
-   
-      <q-list padding>
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon class="" name="book" />
-          </q-item-section>
+  <q-list padding>
+    <q-item
+      :active="activeSideBar === 'profile'"
+      @click="activeSideBar = 'profile'"
+      clickable
+      v-ripple
+    >
+      <q-item-section avatar>
+        <q-icon class="" name="person" />
+      </q-item-section>
 
-          <q-item-section> Appointment </q-item-section>
-        </q-item>
+      <q-item-section> Profile </q-item-section>
+    </q-item>
 
-        <q-item :active="false" clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="event" />
-          </q-item-section>
+    <q-item
+      :active="activeSideBar === 'appointment'"
+      @click="activeSideBar = 'appointment'"
+      clickable
+      v-ripple
+    >
+      <q-item-section avatar>
+        <q-icon name="book_online" />
+      </q-item-section>
 
-          <q-item-section> Calendar </q-item-section>
-        </q-item>
+      <q-item-section> Appointments </q-item-section>
+    </q-item>
 
-        <q-item :active="true"  clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="list" />
-          </q-item-section>
+    <q-item
+      :active="activeSideBar === 'records'"
+      @click="activeSideBar = 'records'"
+      clickable
+      v-ripple
+    >
+      <q-item-section avatar>
+        <q-icon name="book_online" />
+      </q-item-section>
 
-          <q-item-section> Logs </q-item-section>
-        </q-item>
+      <q-item-section> Records </q-item-section>
+    </q-item>
 
-        <q-item clickable class="" v-ripple>
-          <q-item-section avatar>
-            <q-icon name="dark_mode" />
-          </q-item-section>
+    <q-item clickable class="" v-ripple>
+      <q-item-section avatar>
+        <q-icon name="dark_mode" />
+      </q-item-section>
 
-          <q-item-section> Dark Mode </q-item-section>
-          <q-item-section>
-            <q-toggle v-model="value" color="primary" @click=" $q.dark.toggle();"/>
-          </q-item-section>
-        </q-item>
+      <q-item-section> Dark Mode </q-item-section>
+      <q-item-section>
+        <q-toggle v-model="value" color="primary" @click="$q.dark.toggle()" />
+      </q-item-section>
+    </q-item>
 
-        <q-item >
-   
+    <q-item
+      to="admin"
+      clickable
+      v-ripple
+    >
+      <q-item-section avatar>
+        <q-icon name="admin_panel_settings" />
+      </q-item-section>
 
-          <q-item-section> 
-            <AuthDialog>Account</AuthDialog>
-           
-          </q-item-section>
-        </q-item>
-        <hr>
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="login" />
-          </q-item-section>
+      <q-item-section> Administratoe </q-item-section>
+    </q-item>
 
-          <q-item-section> 
-            <LoginView>Sign in</LoginView>
-           
-          </q-item-section>
-        </q-item>
-        <q-item clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="person_add_alt" />
-          </q-item-section>
 
-          <q-item-section> 
-            <RegisterView > Register</RegisterView>
-           
-          </q-item-section>
-        </q-item>
-      </q-list>
-   
+    <q-item>
+      <q-item-section>
+        <AuthDialog>Account</AuthDialog>
+      </q-item-section>
+    </q-item>
+    <hr />
+    <div v-if="!authStore.user">
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon name="login" />
+        </q-item-section>
+
+        <q-item-section>
+          <LoginView>Sign in</LoginView>
+        </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon name="person_add_alt" />
+        </q-item-section>
+
+        <q-item-section>
+          <RegisterView> Register</RegisterView>
+        </q-item-section>
+      </q-item>
+    </div>
+    <div v-else>
+      <q-item clickable @click="authStore.handleLogout" v-ripple>
+        <q-item-section avatar>
+          <q-icon name="logout" />
+        </q-item-section>
+
+        <q-item-section> Logout </q-item-section>
+      </q-item>
+    </div>
+  </q-list>
 </template>
 
 <script>
-import LoginView from '@/views/home/auth/LoginView';
-import RegisterView from '@/views/home/auth/RegisterView.vue';
+import LoginView from "@/views/home/auth/LoginView";
+import RegisterView from "@/views/home/auth/RegisterView.vue";
+import { ref } from "vue";
+import { useAuthStore } from "@/store/auth";
 export default {
-    components:{LoginView, RegisterView},
-   
-    setup(){
-    
-    }
+  components: { LoginView, RegisterView },
 
-}
+  setup() {
+    const authStore = useAuthStore();
+
+    return {
+      activeSideBar: ref(),
+      authStore,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
