f27bd01331a6a4d5e07ce0d2ea34761c0d224f7a
soybean15 <marlonpadilla1593@gmail.com>
2023-07-13 19:48:10 +0800

    adding profile view

--- src/views/home/user/ProfileView.vue
+++ src/views/home/user/ProfileView.vue
@@ -7,7 +7,7 @@
     </div>
 
     <div class="row">
-      <div class="col-6">
+      <div class="col-7">
         <div class="row items-center justify-between">
           <div class="row items-center">
             <div class="m-2 mr-1 font-bold text-base text-blue-400" color="">
@@ -23,15 +23,65 @@
         </div>
       </div>
 
-      <div class="col-6 bg-surface rounded-md shadow-sm p-3 m-3">
-        <div class="row justify-center">
-          <q-avatar class="self-center" size="100px" color="orange">J</q-avatar>
+      <div class="col-7  row bg-surface rounded-md p-3 m-3 shadow-md">
+        <div class="col-6 border-r  border-gray-400 column">
+          <div class="row justify-center">
+            <q-avatar class="self-center" size="70px" color="orange"
+              >J</q-avatar
+            >
+          </div>
+          <div class="row justify-center">
+            <div class="font-semibold text-lg">{{ fullName }}</div>
+          </div>
+          <div class="row justify-center items-center">
+            <q-icon name="email" size="xs"> </q-icon>
+            <span class="ml-1 text-md font-secondary" v-if="authStore.user">{{
+              authStore.user.email
+            }}</span>
+          </div>
+          <div class="row mt-5">
+            <div class="col-12 row justify-center">
+              <div class="text-lg font-semibold">Appointments</div>
+            </div>
+            <div class="col-5 column items-center">
+              <div class="row text-4xl font-bold">1</div>
+              <div class="row font-secondary">Upcoming</div>
+            </div>
+            <div class="col-2 flex justify-center">
+              <div class="w-0.5 bg-gray-500 py-3 h-full"></div>
+            </div>
+
+            <div class="col-5 column items-center">
+              <div class="row text-4xl font-bold">1</div>
+              <div class="row font-secondary">Completed</div>
+            </div>
+          </div>
         </div>
-        <div class="row justify-center">
-          <div class="font-semibold text-lg">{{ fullName }}</div>
+
+        <div class="col-6 pl-2">
+            <div class="row h-20">
+                <div class="col-6  ">
+                    <div class="row font-secondary">Gender</div>
+                    <div class="row">Male</div>
+                </div>
+                <div class="col-6  ">
+                    <div class="row font-secondary">Birthday</div>
+                    <div class="row">March 15, 2000</div>
+                </div>
+            </div>
+            <div class="row  h-20">
+                <div class="col-6  ">
+                    <div class="row font-secondary">Phone Number</div>
+                    <div class="row">0923423465</div>
+                </div>
+                <div class="col-6  ">
+                    <div class="row font-secondary">Address</div>
+                    <div class="row">Sinasajan Penarada Nueva Ecija</div>
+                </div>
+            </div>
         </div>
       </div>
-      <div class="col-5">
+      <div class="col-4">
         .col-5<br />Subsequent columns continue along the new line.
       </div>
     </div>
