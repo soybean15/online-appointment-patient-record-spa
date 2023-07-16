ecc0e6f0e913067cb316a70a2910c3308abc388c
soybean15 <marlonpadilla1593@gmail.com>
2023-07-16 16:23:43 +0800

    added services page

---
+++ src/store/admin.js
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import router from '../router/index'
+import { useServiceStore } from './service'
+
+export const useAdminStore = defineStore('admin', {
+    state: () => ({
+
+        stateServices:null
+
+
+    
+    }),
+    getters: {
+        service:(state)=>state.stateServices
+   
+        },
+    actions: {
+
+        getServices(){
+
+            const serviceStore = useServiceStore()
+            serviceStore.getServices()
+            this.stateServices = serviceStore.services
+
+
+        }
+        
+        
+
+    },
+})
---
+++ src/store/service.js
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import router from '../router/index'
+
+export const useServiceStore = defineStore('service', {
+    state: () => ({
+
+        stateServices:null
+    }),
+    getters: {
+        services:(state)=>state.stateServices
+
+    },
+    actions: {
+
+        async getServices(){
+            const data = await axios.get('api/services')
+            this.stateServices =data.data.services
+        }
+
+
+
+    },
+})
