5e5a9879f78cfc1bacea006b311a41a4b488148f
soybean15 <marlonpadilla1593@gmail.com>
2023-07-17 20:56:00 +0800

    added action column

--- src/views/admin/service/ServiceView.vue
+++ src/views/admin/service/ServiceView.vue
@@ -25,12 +25,14 @@
     </div>
 
     <div class="q-pa-md">
-      <q-table
-        title="Services"
-        :rows="rows"
-        :columns="columns"
-        row-key="name"
-      />
+      <q-table title="Services" :rows="rows" :columns="columns" row-key="name">
+        <template v-slot:body-cell-actions="props">
+          <q-td :props="props">
+            <q-btn icon="mode_edit" @click="onEdit(props.row)"></q-btn>
+            <q-btn icon="delete" @click="onDelete(props.row)"></q-btn>
+          </q-td>
+        </template>
+      </q-table>
     </div>
   </div>
 </template>
@@ -83,6 +85,10 @@ export default {
       serviceStore,
       columns,
       rows,
+      onEdit: (row) => {
+        console.log(row)
+      },
+      onDelete: (row) => {},
     };
   },
 };
