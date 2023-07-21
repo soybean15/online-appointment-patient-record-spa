588f6ef8e1d51cdf059d3b4aad430c15da70452b
Jean Esteban <jean@pop-os.localdomain>
2023-07-21 23:00:17 +0800

    About Page ongoing

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,122 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+    <q-page class="" style="font-family: Georgia, 'Times New Roman', Times, serif;">
+
+<div class="row bg-primary q-pa-xl justify-center">
+  <div class="q-ma-xl ">
+    <div class="text-h4">About Us</div>
+     <div class="text-h6 row justify-center"> Read More</div>
+    </div>     
+</div>
+
+
+<div class="row  q-pa-xl justify-center">
+  <div class="col gt-sm"></div>
+  <div class=" col q-px-lg justify-center text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.      
+   </div> 
+  <div class="col gt-sm"></div>    
+</div>
+
+
+
+<!-- History -->
+<div class="row q-my-md q-pa-xl bg-yellow-1 text-gray-700">
+  <div class="col items-center q-py-lg q-px-md justify-center">
+      <div class="row justify-center"> 
+        <img src="../assets/image1.jpg"  style="width: 600px; height: 400px; border-radius: 5px;">
+      </div>
+  </div>
+  <div class="col q-py-md">
+    <div class="row text-h4">History
+    </div>
+    <div class="row q-py-md q-mr-xl q-pr-md">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.           
+    </div>
+  </div>
+</div>
+
+<!-- Mission Vision -->
+<div class="row q-my-md q-pa-xl">     
+  <div class="col q-py-md justify-center q-pl-xl">
+    <div class="row text-h4 text-red-600 q-pl-xl">Mission </div>
+    <div class="row q-py-md q-mr-xl q-pl-xl">
+          To provide accessible, affordable and maximum quality of maternal care, child health and family planning services in a committed, competent and compassionate manner
+    </div>
+    <div class="row text-h4 text-blue-800 q-pl-xl">Vision </div>
+    <div class="row q-py-md q-mr-xl q-pl-xl">
+      To achieve continuous maximum delivery of quality health care services that will lessen maternal and child morbidity and mortality with improved family planning services in order to attain a better and happy community    </div>
+    </div>
+
+  <div class="col items-center q-py-sm q-px-md justify-center">
+      <div class="row justify-center"> 
+        <img src="../assets/img/logo/logo.png"  style="width: 450px; height: 400px; border-radius: 5px;">
+      </div>
   </div> 
+</div>
+
+
+<!-- Doctors -->
+
+<div class="row justify-center  q-mt-md q-pa-md bg-yellow-1 text-gray-700 text-h4">Doctors</div>
+<div class="row q-px-md bg-yellow-1 text-gray-700 ">
+  
+  <div class="col items-center q-py-md q-px-md justify-center">
+      <div class="row justify-center q-px-lg"> 
+        <img src="../assets/img/logo/doctor1.png"  style="width: 300px; height: 200px; border-radius: 5px">
+      </div>
+  </div>
+  <div class="col q-py-xl">
+      <div class="row text-h6 text-bold">Dr. Emerson DC. Garcia</div>
+      <div class="row q-mr-xl q-pr-md text-h7" >UROSURGEON </div>
+  </div>
+</div>
+
+  <div class="row bg-yellow-1 text-gray-700 q-pb-lg">
+      <div class="col items-center q-py-md q-px-md justify-center">
+          <div class="row justify-center "> 
+            <img src="../assets/img/logo/doctor3.png"  style="width: 300px; height: 200px; border-radius: 5px;">
+          </div>
+      </div>
+      <div class="col q-py-xl">
+        <div class="row text-h6 text-bold">Dra. Kristina T. Vergara-Garcia</div>
+        <div class="row q-mr-xl q-pr-md text-h7" >OBSTETRICIAN/GYNECOLOGIST </div>
+      </div>
+  </div>
+
+
+<!-- Branches -->
+<div class="row q-my-md q-pa-xl"> 
+     
+  <div class="col q-py-md justify-center q-pl-xl">
+        <div class="row text-h4 q-pa-lg" >Branches</div>
+        <div class="row text-h5 q-pl-xl q-mt-lg">
+          <q-icon name="location_on"/>Cabiao Branch 
+        </div>
+        <div class="row q-py-sm q-mr-xl q-pl-xl">
+          Jose Abad Santos Avenue, San Fernando Sur, Cabiao, Philippines, 3107   
+         </div>
+        
+        <div class="row text-h5 q-pl-xl q-mt-lg">
+          <q-icon name="location_on"/>Jaen Branch 
+        </div>
+        <div class="row q-py-sm q-mr-xl q-pl-xl">
+          Jose Abad Santos Avenue, San Fernando Sur, Cabiao, Philippines, 3107    </div>
+  </div>
+
+  <div class="col items-center q-py-sm q-px-md justify-center">
+      <div class="row justify-center"> 
+        <img src="../assets/img/logo/clinic3.jpg"  style="width: 450px; height: 400px; border-radius: 5px;">
+      </div>
+  </div> 
+</div>
+
+
+
+
+
+
+
+</q-page>
 </template>
