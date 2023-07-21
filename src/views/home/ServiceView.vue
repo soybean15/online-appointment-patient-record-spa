3205197417b6b579c22f074edeb428e9efd3f6a9
soybean15 <marlonpadilla1593@gmail.com>
2023-07-21 10:42:19 +0800

    added service page

---
+++ src/views/home/ServiceView.vue
@@ -0,0 +1,33 @@
+<template>
+ <div>Services</div>
+ <ul>
+    <li v-for="service in services" :key="service.id">
+    
+        {{ service.name }}   {{ service.price }} 
+    </li>
+ </ul>
+</template>
+
+<script>
+import {useServiceStore} from '@/store/service'
+import { onMounted } from 'vue'
+export default {
+
+    setup(){
+        const serviceStore = useServiceStore()
+
+        onMounted(()=>{
+            serviceStore.getServices()
+        })
+
+        return{
+            services: serviceStore.services
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
