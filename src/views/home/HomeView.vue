1a8428655bc8e6513dd256c997ba9f3d7561ac1e
Jean Esteban <jean@pop-os.localdomain>
2023-07-24 20:23:58 +0800

    Landing page

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
 
     <q-page class="">
-        <!-- Header -->
-        <div class="row bg-cyan-5">
+        <!-- Hero Section -->
+        <div class="row bg-teal-4">
             <!-- Maxilife Multispecialty Clinic and Diagnostic Center -->
             <div class="col text-white">
                 <div style="text-align:center; max-width: 650px; margin-right: auto; margin-left: auto;" class="q-pt-md  ">
@@ -11,7 +11,7 @@
                     <div class="text-h6 q-px-lg q-pb-sm"> Book your appointment from the comfort 
                         of your own home and receive quality care from our expert team</div>
                     <div class=" bg-white text-grey mb-5 q-px-xl q-my-md q-mb-md rounded-lg ">Set an Apppointment
-                        <q-btn color="blue" 
+                        <q-btn color="cyan" 
                             icon-right="calendar_today" 
                             label="" 
                             class="q-my-sm "
@@ -19,9 +19,9 @@
                     </div>
                 </div>
             </div>
-            <div class="col gt-sm q-px-sm  q-mx-xl q-mt-sm opacity-80">
+            <div class="col gt-sm q-px-sm q-py-xl q-mx-xl  opacity-85">
                 <q-img src="../../assets/img/images/doctor2.png" 
-                style="max-height:650px; margin-left:auto; margin-right: auto; display: block; border-radius: 10px; width:500px; height: 500px; "></q-img>
+                style="max-height:650px; margin-left:auto; margin-right: auto; display: block; border-radius: 10px; width:500px; height: 550px; "></q-img>
                 
             </div>
 
@@ -29,54 +29,156 @@
 
         <!-- Second Row -->
         <div class="q-mx-xl q-px-xl ">
-        <div class="row justify-center text-h5 text-bold q-px-md q-pt-md q-my-lg ">Offered</div>
+            <div class="row justify-center text-h6 q-py-xl q-pt-md q-my-lg ">Our Best Offered</div>            
+            <div class="row q-py-sm q-px-lg justify-evenly" >
+            
+                <div class="col-12 col-md q-px-xl q-pt-md "  >
+                    <div class="flex-col" >
+                        <q-card class=" text-gray bg-white">
+                            <div class="flex justify-center ">
+                                <q-img src="../../assets/img/images/facility1.jpg" style="max-height: 200px; height: 200px;"></q-img>
+                            </div>                                                      
+                        </q-card>   
+                        <div class="text-h7 q-ml-sm q-py-md">
+                            <div class="text-h6 q-my-sm text-amber-9">
+                                Well cleaned and neat facility 
+                            </div>
+                            <div class="q-pr-md">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+                            </div>
+                        </div>    
+                    </div>               
+                </div>
+            
 
-        <div class="row q-py-sm q-px-xl q-mx-xl" >
-          
-
-            <div class="col-6 col-md"  >
-                <div class="flex-col" >
-                    <div class="flex justify-center ">
-                            <q-icon name="night_shelter" color="teal" size="60px" style="padding: 20px;"/>
-                    </div>                      
-                            <div class="text-h7 flex justify-center q-py-md">Well cleaned and neat facility</div>                 
-                    </div>
-            </div>
-
-            <div class="col-6 col-md q-px-xl ">
-                <div class="flex-col  ">
-                    <div class="flex justify-center">
-                            <q-icon name="medication" color="warning" size="60px" style="padding: 20px;"/>
-                    </div>                      
-                            <div class="text-h7 flex justify-center q-py-md">Well cleaned and neat facility</div>                 
-                    </div>
-            </div>
-
-            <div class="col-6 col-md ">
-                <div class="flex-col   ">
-                    <div class="flex justify-center">
-                            <q-icon name="night_shelter" color="deep-orange" size="60px" style="padding: 20px;"/>
-                    </div>                      
-                            <div class="text-h7 flex justify-center  q-py-md">Well cleaned and neat facility</div>                 
-                    </div>
-            </div>
-
-            <div class="col-6 col-md ">
-                <div class="flex-col  ">
-                    <div class="flex justify-center">
-                            <q-icon name="night_shelter" color="deep-orange" size="60px" style="padding: 20px;"/>
-                    </div>                      
-                            <div class="text-h7 flex justify-center  q-py-md">Well cleaned and neat facility</div>                 
-                    </div>
-            </div>
-       
-
-        </div>
-    </div>
+                <div class="col-12 col-md q-px-xl q-pt-md "  >
+                    <div class="flex-col" >
+                        <q-card class=" text-gray bg-white">
+                            <div class="flex justify-center ">
+                                <q-img src="../../assets/img/images/service1.jpg" style="max-height: 200px; height: 200px;"></q-img>
+                            </div>                                                      
+                        </q-card>   
+                        <div class="text-h7 q-ml-sm q-py-md">
+                            <div class="text-h6 q-my-sm text-amber-9">
+                                Affordable and Quality Services 
+                            </div>
+                            <div class="q-pr-md">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+                            </div>
+                        </div>    
+                    </div>               
+                </div>
 
 
+                <div class="col-12 col-md q-px-xl q-pt-md "  >
+                    <div class="flex-col" >
+                        <q-card class=" text-gray bg-white">
+                            <div class="flex justify-center ">
+                                <q-img src="../../assets/img/images/maxilife-staff.jpg" style="max-height: 200px; height: 200px;"></q-img>
+                            </div>                                                      
+                        </q-card>   
+                        <div class="text-h7 q-ml-sm q-py-md">
+                            <div class="text-h6 q-my-sm text-amber-9">
+                                Best healthcare providers
+                            </div>
+                            <div class="q-pr-md">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+                            </div>
+                        </div>    
+                    </div>               
+                </div>
+
+
+            
+
+
+
+            
+    
+            
         
 
+            </div>
+        </div>
+
+        
+        <div class="row justify-center text-h6 q-py-xl q-pt-md q-mt-lg ">Book your Appointment </div>
+        <div class="bg-teal-1 q-ma-xl">
+            
+
+
+
+
+            <div class="col bg-teal-2">
+                <q-img src="../../assets/img/images/doctor4.jpg" class="w-full opacity-70 " style="height: 700px;">
+                    <div class="row">
+                    <div class="col-12 col-md  justify-center q-pa-xl ">
+                        <div class="row opacity-75 q-pt-xl q-pl-md">
+                            <img src="../../assets/img/images/logo1.png" style="width: 130px; height: 100px;">
+                        </div>
+                        <div class="row text-h2 text-white q-py-lg q-px-xl">
+                            Maxilife Multispecialty Clinic
+                        </div>
+                        <div class="row q-py-lg q-pl-xl q-mb-xl ">
+                            <div class="text-white q-py-md q-px-md bg-teal-5 text-bold ">Book Appointment</div>
+                        </div>
+                 </div>
+                    
+                  <div class="col-12 col-md  justify-center q-pa-xl ">
+                        <div class="row opacity-75 q-pt-xl q-pl-md">
+                            <img src="../../assets/img/images/logo1.png" style="width: 130px; height: 100px;">
+                        </div>
+                        <div class="row text-h2 text-white q-py-lg q-px-xl">
+                            Maxilife Multispecialty Clinic
+                        </div>
+                        <div class="row q-py-lg q-pl-xl q-mb-xl">
+                            <div class="text-teal q-py-md q-px-md bg-white text-bold ">Book Appointment</div>
+                        </div>
+                  </div>
+</div>
+                
+                </q-img>
+
+                
+            </div>
+        </div>
+        
+      <!-- Third Section -->
+      <div class="">
+        <div class="row justify-center text-h6 q-py-xl q-pt-md q-mt-lg ">Book your Appointment </div>
+        <div class="row q-pa-xl">
+        <div class="col-12 col-md bg-teal-4 justify-center q-pa-xl">
+            <div class="row opacity-75 q-pt-xl q-pl-md">
+                <img src="../../assets/img/images/logo1.png" style="width: 130px; height: 100px;">
+            </div>
+            <div class="row text-h2 text-white q-py-lg q-px-xl">
+                Maxilife Multispecialty Clinic
+            </div>
+            <div class="row q-py-lg q-pl-xl q-mb-xl">
+                <div class="text-teal q-py-md q-px-md bg-white text-bold ">Book Appointment</div>
+            </div>
+        </div>
+
+        <div class="col-12 col-md bg-teal-1 opacity-50">
+        <q-img src="../../assets/img/images/doctor4.jpg">
+          <div class="absolute-full col ">
+            <div class="row justify-center text-h4 text-white q-py-xl text-bold">How it works?</div>
+            <div class="row">
+                  <ul>
+                <li>fks</li>
+                <li>fks</li>
+                <li>fks</li>
+                <li>fks</li>
+                <li>fks</li>
+            </ul>  
+            </div>
+          </div>
+        </q-img>
+      </div>
+
+        </div>
+
+      </div>
 
         
     </q-page>
