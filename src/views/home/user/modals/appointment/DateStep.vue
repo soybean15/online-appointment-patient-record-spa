e7e542bc84dac5cfae09fa472523b4429a479b69
soybean15 <marlonpadilla1593@gmail.com>
2023-07-27 20:19:34 +0800

    added calendar functionality

--- src/views/home/user/modals/appointment/DateStep.vue
+++ src/views/home/user/modals/appointment/DateStep.vue
@@ -1,31 +1,63 @@
 <template>
-    <div >
-    
-        <q-date
-         style="width: 100%;"
-          v-model="date"
-          :events="events"
-          landscape
-        />
-  
-      </div>
-    
-  </template>
+  <div>
+    <q-date style="width: 100%" v-model="currentDate" :events="events" :options="allDatesInCurrentMonth" landscape />
+  </div>
+</template>
   
   <script>
-  import { ref } from 'vue'
-  
-  export default {
-    setup () {
-      return {
-        date: ref('2019/02/01'),
-        events: [ '2019/02/01', '2019/02/05', '2019/02/06', '2019/02/09', '2019/02/23' ],
-        eventsFn (date) {
-          const parts = date.split('/')
-          return parts[ 2 ] % 2 === 0
+import { ref } from "vue";
+import { useAppointmentStore } from "@/store/appointment";
+import { eachDayOfInterval,format } from 'date-fns';
+export default {
+  setup() {
+    const appointmentStore = useAppointmentStore();
+    const allDatesInCurrentMonth = ref([])
+
+    const currentDate = ref(format(new Date(), 'yyyy-MM-dd'));
+
+    const eventsFn = () => {
+        const currentDate = new Date();
+      const currentYear = currentDate.getFullYear();
+      const currentMonth = currentDate.getMonth();
+
+
+
+      const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+
+      
+      const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
+
+      const allDates = [];
+      for (let date = firstDayOfMonth; date <= lastDayOfMonth; date.setDate(date.getDate() + 1)) {
+        const day =date.getDay()
+        let isDateValid =  appointmentStore.selectedDoctor.schedules.some(schedule => schedule.day_of_week === day);
+      
+        if(isDateValid ){
+            allDates.push(format(new Date(date), 'yyyy/MM/dd'));
         }
+        
       }
-    }
-  }
-  </script>
+
+      // Save all dates in the component's data
+      allDatesInCurrentMonth.value = allDates
+    };
+    eventsFn()
+    console.log(appointmentStore.selectedDoctor.schedules)
+    console.log(allDatesInCurrentMonth.value)
+
+    return {
+        currentDate,
+      events: [
+        "2019/02/01",
+        "2019/02/05",
+        "2019/02/06",
+        "2019/02/09",
+        "2019/02/23",
+      ],
+      eventsFn,
+      allDatesInCurrentMonth
+    };
+  },
+};
+</script>
   
