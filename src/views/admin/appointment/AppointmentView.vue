465a08dbf23079dc46c78ea581096bc9254d209e
soybean15 <marlonpadilla1593@gmail.com>
2023-07-30 19:19:53 +0800

    starts admin appointment

---
+++ src/views/admin/appointment/AppointmentView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+
+    <div class="col-8">
+        <TablesContainer/>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import TablesContainer from './components/TablesContainer.vue';
+export default {
+    components:{
+        TablesContainer
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/admin/appointment/components/TablesContainer.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="q-pa-md ">
+        <q-card>
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="mails" label="Mails" />
+          <q-tab name="alarms" label="Alarms" />
+          <q-tab name="movies" label="Movies" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="mails">
+          <AppointmentTable/>
+          </q-tab-panel>
+
+          <q-tab-panel name="alarms">
+            <div class="text-h6">Alarms</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="movies">
+            <div class="text-h6">Movies</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
+
+    </div>
+  </template>
+  
+  <script>
+  import AppointmentTable from './AppointmentTable.vue';
+  import { ref } from 'vue'
+  
+  export default {
+    components:{AppointmentTable},
+    setup () {
+      return {
+        tab: ref('mails'),
+        splitterModel: ref(20)
+      }
+    }
+  }
+  </script>
+  
