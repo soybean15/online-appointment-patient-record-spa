1a7804cb4440521c3954a3e36d24ba6d04cc6d30
soybean15 <marlonpadilla1593@gmail.com>
2023-07-31 21:40:46 +0800

    created approve and reject function

--- src/views/admin/appointment/components/AppointmentTable.vue
+++ src/views/admin/appointment/components/AppointmentTable.vue
@@ -1,95 +1,129 @@
 <template>
-    <div class="" v-if="appointmentStore.pending">
-      <q-table
-        color="primary"
-        card-class=" text-brown"
-        table-class="text-grey-8"
-        table-header-class="text-brown"
-        flat
-        bordered
-        title="Treats"
-       :rows="appointmentStore.pending.data"
-        :columns="columns"
-        row-key="name"
-      >
+  <div class="" v-if="appointmentStore.pending">
+    <q-table
+      color="primary"
+      card-class=" text-brown"
+      table-class="text-grey-8"
+      table-header-class="text-brown"
+      flat
+      bordered
+      title="Treats"
+      :rows="appointmentStore.pending.data"
+      :columns="columns"
+      row-key="name"
+    >
       <template v-slot:top-right>
         <q-pagination
           v-model="current"
           color="primary"
-          :max="appointmentStore.pending.last_page "
+          :max="appointmentStore.pending.last_page"
           :max-pages="5"
           boundary-numbers
         />
       </template>
-    
+
+      <template v-slot:body-cell-image="props">
+        <q-td :props="props">
+          <img
+            :src="props.row.user.profile[0].image"
+            alt="Profile Image"
+            style="width: 40px; height: 40px"
+          />
+        </q-td>
+      </template>
+
+      <template v-slot:body-cell-actions="props">
+        <q-td :props="props">
+        
+          <q-btn dense color="green" @click="appointmentStore.approve(props.row)" icon-right="done_all" label="Approve" />
+
+          <q-btn dense color="red"   @click="appointmentStore.reject(props.row)" icon-right="cancel" label="Decline" />
+        </q-td>
+      </template>
     </q-table>
-    </div>
-  </template>
+  </div>
+</template>
   
   <script>
-  import {useAppointmentStore} from '@/store/adminAppointment'
-  const columns = [
-    {
-      name: 'name',
-      required: true,
-      label: 'Patient Name',
-      align: 'left',
-      field: row => row.user.profile[0].full_name,
-      format: val => `${val}`,
-      sortable: true
+import { useAppointmentStore } from "@/store/adminAppointment";
+const columns = [
+  {
+    name: "image",
+    required: true,
+    label: "",
+    align: "center",
+    field: (row) => row.user.profile[0].image,
+    format: (val) => `${val}`,
+    sortable: false,
+    "q-table-col-props": {
+      "class-name": "q-py-xs",
+      "style-name": "width: 60px",
     },
-    {
-      name: 'service',
-      required: true,
-      label: 'Service',
-      align: 'left',
-      field: row => row.service.name,
-      format: val => `${val}`,
-      
-    },
+  },
+  {
+    name: "name",
+    required: true,
+    label: "Patient Name",
+    align: "left",
+    field: (row) => row.user.profile[0].full_name,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+  {
+    name: "service",
+    required: true,
+    label: "Service",
+    align: "left",
+    field: (row) => row.service.name,
+    format: (val) => `${val}`,
+  },
 
-    {
-      name: 'schedule',
-      required: true,
-      label: 'Schedule Date',
-      align: 'left',
-      field: row => row.schedule_date,
-      format: val => `${val}`,
-      sortable: true
-    },
-    {
-      name: 'contact',
-      required: true,
-      label: 'Contact',
-      align: 'left',
-      field: row => row.user.profile[0].contact_number,
-      format: val => `${val}`,
-      
-    },
-    {
-      name: 'doctor',
-      required: true,
-      label: 'Assigned Doctor',
-      align: 'left',
-      field: row => row.doctor.full_name,
-      format: val => `${val}`,
-      
-    },
-   
-   
-  ]
-  
-  
-  export default {
-    setup () {
-        const appointmentStore = useAppointmentStore()
+  {
+    name: "schedule",
+    required: true,
+    label: "Schedule Date",
+    align: "left",
+    field: (row) => row.schedule_date,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+  {
+    name: "contact",
+    required: true,
+    label: "Contact",
+    align: "left",
+    field: (row) => row.user.profile[0].contact_number,
+    format: (val) => `${val}`,
+  },
+  {
+    name: "doctor",
+    required: true,
+    label: "Assigned Doctor",
+    align: "left",
+    field: (row) => row.doctor.full_name,
+    format: (val) => `${val}`,
+  },
+  {
+    name: "actions",
+    required: true,
+    label: "Actions",
+    align: "center",
+  },
+];
 
-      return {
-        columns,
-        appointmentStore,
-        
+export default {
+  setup() {
+    const appointmentStore = useAppointmentStore();
+
+    return {
+      columns,
+      appointmentStore,
+      onClick:(row)=>{
+       //testing kung tama ung naclick
+        console.log(row)
       }
-    }
-  }
-  </script>
+    };
+  },
+};
+</script>
   
