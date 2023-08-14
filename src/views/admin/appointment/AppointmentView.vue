3f619c42ceb6a71710c6adfda3fd28d3e767403f
soybean15 <marlonpadilla1593@gmail.com>
2023-08-14 14:18:59 +0800

    finished change primary color

--- src/views/admin/appointment/AppointmentView.vue
+++ src/views/admin/appointment/AppointmentView.vue
@@ -1,34 +1,31 @@
 <template>
-  <div class="row">
-
-    <div class="col-12">
-        <TablesContainer/>
+  <div class="column">
+    <div class="col text-xl py-3 px-5">Appointments</div>
+    <div class="row">
+      <div class="col-12">
+        <TablesContainer />
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import TablesContainer from './components/TablesContainer.vue';
-import {useAppointmentStore} from '@/store/adminAppointment'
-import { onMounted } from 'vue';
+import TablesContainer from "./components/TablesContainer.vue";
+import { useAppointmentStore } from "@/store/adminAppointment";
+import { onMounted } from "vue";
 export default {
-    components:{
-        TablesContainer
-    },
-    setup(){
-        const appointmentStore = useAppointmentStore()
+  components: {
+    TablesContainer,
+  },
+  setup() {
+    const appointmentStore = useAppointmentStore();
 
-        onMounted(()=>{
-            appointmentStore.index()
-        })
-
-
-    }
-
-}
+    onMounted(() => {
+      appointmentStore.index();
+    });
+  },
+};
 </script>
 
 <style>
-
 </style>
